80cf1f985508f75cbfeb3d8c1eb859d701c7c309
EC2 Default User <nbomasi>
2023-01-04 09:59:57 +0000

    updating my doc to git

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.24.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nodered_image" {
+  name = "nodered/node-red:latest"
+}
+
+resource "docker_container" "nodered_container" {
+  name  = "boma-nodered"
+  image = docker_image.nodered_image.image_id
+  ports {
+    internal = 1880
+    external = 1880
+  }
+}
+

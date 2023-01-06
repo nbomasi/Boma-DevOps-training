2148034a0f8e2f05a06cab6b2e6dc19aa21e09eb
EC2 Default User <ec2-user@ip-172-31-68-38.ec2.internal>
2023-01-06 17:28:34 +0000

    editing files

--- Docker-provider/main.docker.tf
+++ Docker-provider/main.docker.tf
@@ -7,21 +7,37 @@ terraform {
   }
 }
 
-provider docker {}
+provider "docker" {}
 
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
+
+resource "random_string" "random" {
+  length  = 4
+  special = false
+}
+
 resource "docker_container" "nodered_container" {
   name  = "boma-nodered"
   image = docker_image.nodered_image.image_id
   ports {
     internal = 1880
-    external = 1880
+    # external = 1880
+  }
+}
+
+resource "docker_container" "nodered_container2" {
+  name  = "boma-nodered2"
+  image = docker_image.nodered_image.image_id
+  ports {
+    internal = 1880
+    # external = 1880
   }
 }
 
 output "Ip-Address" {
-  value = docker_container.nodered_container.ip_address
+  value       = join(" : ", [docker_container.nodered_container.ip_address, docker_container.nodered_container.ports[0].internal])
   description = "ip addres of my container"
 }
+

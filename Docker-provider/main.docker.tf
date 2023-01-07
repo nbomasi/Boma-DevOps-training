2428cf5ae5d055223b856a7bd08118e1db8dbce5
nbomasi <nbomasi@gmail.com>
2023-01-07 03:53:41 +0000

    editing

--- Docker-provider/main.docker.tf
+++ Docker-provider/main.docker.tf
@@ -27,14 +27,14 @@ resource "docker_container" "nodered_container" {
   }
 }
 
-# resource "docker_container" "nodered_container2" {
-#   name  = "boma-nodered2"
-#   image = docker_image.nodered_image.image_id
-#   ports {
-#     internal = 1880
-#     # external = 1880
-#   }
-# }
+resource "docker_container" "nodered_container2" {
+  name  = "boma-nodered2"
+  image = docker_image.nodered_image.image_id
+  ports {
+    internal = 1880
+    external = 1800
+  }
+}
 
 output "Ip-Address" {
   value       = join(" : ", [docker_container.nodered_container.ip_address, docker_container.nodered_container.ports[0].internal])
--- Training-Docs.md
+++ Training-Docs.md
@@ -1,4 +1,8 @@
-# Documentation for my Terraform Traing
+# Documentation for my Terraform Traning:
+
+Terraform installation: link (https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli#install-cli)
+
+Useful Terraform link: 	https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs
 
 Note that for docker to images for be deploy through windows, **docker desktop** have to be downloaded
 
